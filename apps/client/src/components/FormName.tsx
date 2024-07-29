287d2d0fb6122d96bffe0a3f6016a5ca0dd881e7
Jesse <45066435+jesse-mtm@users.noreply.github.com>
2024-07-30 01:10:44 +0200

    update

--- apps/client/src/components/FormName.tsx
+++ apps/client/src/components/FormName.tsx
@@ -1,18 +1,17 @@
 import * as React from 'react';
 import { BookingContext } from 'src/context/BookingContext';
-import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
 
 export const FormName = () => {
   const [booking, setBooking] = React.useContext(BookingContext);
   if (!booking) return null;
 
   return (
-    <Input
+    <Textarea
       id="name"
-      type="text"
-      className="flex w-full text-2xl font-bold p-0 border-0 focus-visible:ring-0 shadow-none"
-      value={booking.name}
       placeholder="Enter a name"
+      className="flex w-full max-h-[72px]  text-3xl font-bold p-0 border-0 focus-visible:ring-0 shadow-none resize-none overflow-y-visible"
+      value={booking.name}
       onChange={(event) => setBooking({ ...booking, name: event.target.value })}
     />
   );
