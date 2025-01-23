1134c2cd465dea3c11b74faeaca04f30a94077de
Jesse <45066435+jesse-mtm@users.noreply.github.com>
2025-01-23 21:00:08 +0100

    After migrating from hoteltrinserhof@gmail.com to hotel@trinserhof.com

--- packages/constants/src/FIREBASE_CONFIG.ts
+++ packages/constants/src/FIREBASE_CONFIG.ts
@@ -1,10 +1,9 @@
 export const FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyA9BtOR-U15YjkWksOyzx329ExQF4vjiVc',
-  appId: '1:610796710348:web:eaa0c623aa77451ae1d569',
-  authDomain: 'hotel-trinserhof.firebaseapp.com',
-  databaseURL: 'https://hotel-trinserhof-default-rtdb.europe-west1.firebasedatabase.app',
-  measurementId: 'G-FYXT53SHJQ',
-  messagingSenderId: '610796710348',
-  projectId: 'hotel-trinserhof',
-  storageBucket: 'hotel-trinserhof.appspot.com',
+  apiKey: 'AIzaSyCnUaGY69oeR5YdR9OwyguYPgGB0RyLoWs',
+  appId: '1:1043164637160:web:3e88b90f9c5f1f0b66e65d',
+  authDomain: 'trinserhof-bookings.firebaseapp.com',
+  databaseURL: 'https://trinserhof-bookings-default-rtdb.europe-west1.firebasedatabase.app',
+  messagingSenderId: '1043164637160',
+  projectId: 'trinserhof-bookings',
+  storageBucket: 'trinserhof-bookings.firebasestorage.app',
 };
