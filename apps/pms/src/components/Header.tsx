f0f77e89fda0a576b0faef769320466a7a951f02
jesse <jesse1k87@gmail.com>
2026-06-24 14:45:34 +0200

    Remove logo in top-left

--- apps/pms/src/components/Header.tsx
+++ apps/pms/src/components/Header.tsx
@@ -15,11 +15,6 @@ export const Header = ({ navMenu }: HeaderProps) => {
     <div className="sticky top-0 z-30 flex flex-row w-full items-center content-center gap-2 p-2 bg-background border-b">
       <div className="flex flex-row gap-1 sm:gap-2 items-center content-center shrink-0 mx-1">
         {navMenu}
-        <img
-          src="./trinserhof-logo.svg"
-          alt="Hotel Trinserhof"
-          className="hidden sm:block h-6 sm:h-8"
-        />
       </div>
       <div className="flex flex-1 min-w-0 items-center content-center justify-end mx-1">
         {searchOpen ? (
