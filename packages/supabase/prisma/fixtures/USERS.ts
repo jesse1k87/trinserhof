57c70be4e17b9e0483add5d94e3bc9d363f065a7
jesse <jesse1k87@gmail.com>
2026-06-28 17:06:09 +0200

    add users to seed

--- packages/supabase/prisma/fixtures/USERS.ts
+++ packages/supabase/prisma/fixtures/USERS.ts
@@ -2,7 +2,15 @@ import { type Role } from '@trinserhof/types';
 
 export const USERS: { email: string; role: Role }[] = [
   {
+    email: 'hotel@trinserhof.com',
+    role: 'READER',
+  },
+  {
     email: 'jesse1k87@gmail.com',
     role: 'OWNER',
+  },
+  {
+    email: 'jennifer.m.covi@gmail.com',
+    role: 'MANAGER',
   },
 ];
